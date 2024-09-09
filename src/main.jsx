b7092d59858e764a77c707e30b52c7e2fb4229e9
Josh El <emmanueljoshua712@gmail.com>
2024-09-09 09:23:00 +0100

    feat: remove style  sheet import
    feat: change doc query method

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./Index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.querySelector("#root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
