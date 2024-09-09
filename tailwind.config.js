e79e77024f8fad50986228abc1d11d5bfb5cb6de
Josh El <emmanueljoshua712@gmail.com>
2024-09-09 08:40:31 +0100

    setup: create tailwind configs

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export const purge = ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"];
+export const darkMode = false;
+export const theme = {
+  extend: {
+    fontFamily: {
+      jost: ["Jost", "sans-serif"],
+    },
+  },
+};
+export const variants = {
+  extend: {},
+};
+export const plugins = [];
